d5956c5b963be403564987a8339a68b091bf9e43
Murray Butler <s16113916@bcud2-010038.local>
2019-11-11 10:08:53 +0000

    Remaining foe assets add and player class script created

---
+++ BraggartPort/player.swift
@@ -0,0 +1,21 @@
+//
+//  player.swift
+//  BraggartPort
+//
+//  Created by Murray Butler on 11/11/2019.
+//  Copyright © 2019 Haxolotl. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class Player
+{
+    var playername:String
+    var playerscore:Int
+    init ()
+    {
+        playerscore = 0;
+        self.playername = "FantasyName";        
+    }
+}
