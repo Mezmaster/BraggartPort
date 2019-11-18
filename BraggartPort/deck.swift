8a50c1367717491af436873e984ed3da857bec94
Murray Butler <s16113916@bcud2-010038.local>
2019-11-18 11:14:18 +0000

    -Card type enums added

--- BraggartPort/deck.swift
+++ BraggartPort/deck.swift
@@ -8,20 +8,148 @@
 
 import SpriteKit
 import GameplayKit
-
-class Card
+enum FoeCardType:Int
 {
+    case trout,
+    chicken,
+    urchin,
+    cat,
+    johan,
+    lonelyogre,
+    bard,
+    faeries,
+    dwarf,
+    forest,
+    ghoul,
+    carlo,
+    wizards,
+    highpriest,
+    cultists,
+    ugkrugglug,
+    spirits,
+    raiders,
+    amazon,
+    asssassins,
+    magician,
+    mercanaris,
+    gladiator,
+    fellknight,
+    sandcreatures,
+    undeadking,
+    king,
+    visitor,
+    necromancer,
+    queen,
+    dragon,
+    demon
+}
+enum DeedCardType:Int
+{
+    case wetmyself,
+    screaming,
+    stones,
+    killing,
+    terror,
+    beaten,
+    summoned,
+    captured,
+    pillaged,
+    portrait,
+    opened,
+    sister,
+    married,
+    wokeup,
+    danced,
+    overcame,
+    heroic,
+    defended,
+    barfight,
+    disguised,
+    drunk,
+    ambush,
+    lovepotion,
+    chariot,
+    outwitted,
+    weapons,
+    hat,
+    oil,
+    uprising,
+    princess,
+    elements,
+    slayer
+}
+enum SceneCardType:Int
+{
+    case family,
+    maiden,
+    spell,
+    infiltration,
+    bodyguard,
+    bedchamber,
+    steed,
+    sorcery,
+    temple,
+    bootssmile,
+    battlefield,
+    warlord,
+    frozen,
+    citadel
+}
+enum ResultCardType:Int
+{
+    case crying,
+    smell,
+    banned,
+    betterperson,
+    glow,
+    court,
+    barmaids,
+    knighted,
+    deeds,
+    painting,
+    treasures,
+    armies
+}
+enum NonBoastCardType:Int
+{
+    case liar,
+    outrageousliar,
+    triptothebar,
+    drunkendemands,
+    pickpocket,
+    distractingwench
+}
+class Card : SKSpriteNode
+{
+    var cardname:String
+    var cardsprite:SKTexture
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("NSCoding not supported")
+    }
+    init()
+    {
+        cardname = "Liar"
+        cardsprite = SKTexture(imageNamed: "LiarCard")
+    }
     
 }
 class BoastCard: Card
 {
-    
+    var boastvalue:Int8
+    var finalvalue:Int8
+    override init()
+    {
+        boastvalue = 0
+        finalvalue = 0
+    }
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("NSCoding not supported")
+    }
 }
-class PloyCard: Card
-{
-    
-}
-class LiarCard: Card
+class NonBoastCard: Card
 {
     
 }
